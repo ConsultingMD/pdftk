9ae9b2dc13d5e66c948f34db6f512ff42fe2d052
Justin Ahn <justin@soonahn.ca>
2014-02-21 16:52:15 -0800

    We can't read a FUCKING PATH you idiot

--- lib/pdftk.rb
+++ lib/pdftk.rb
@@ -30,9 +30,7 @@ class Pdftk
     err = Open3.popen3(command) do |stdin, stdout, stderr|
       stderr.read
     end
-    pdf = @merged_file_path.read
     raise "PDF could not be generated!" if pdf and pdf.rstrip.length == 0
-    pdf
   rescue Exception => e
     raise "Failed to execute:\n#{command}\nError: #{e}"
   end
